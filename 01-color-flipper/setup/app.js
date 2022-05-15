c94f7847fa419d5f72d7ba9eda3cdea894568239
Stefano Frontini <stefanofrontini75@gmail.com>
2022-05-15 09:40:17 +0200

    first project done

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+
+let randomItem;
+
+btn.addEventListener("click", function () {
+  // get random number between 0 - 3 colors
+  let random = Math.floor(Math.random() * colors.length);
+
+  while (randomItem === random) {
+    random = Math.floor(Math.random() * colors.length);
+  }
+  randomItem = random;
+
+  color.textContent = colors[randomItem];
+  document.body.style.backgroundColor = colors[randomItem];
+});
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hex.length);
+    hexColor += hex[random];
+  }
+  //   const randomColor = `#${hex[Math.floor(Math.random() * hex.length)]}${
+  //     hex[Math.floor(Math.random() * hex.length)]
+  //   }${hex[Math.floor(Math.random() * hex.length)]}${
+  //     hex[Math.floor(Math.random() * hex.length)]
+  //   }${hex[Math.floor(Math.random() * hex.length)]}${
+  //     hex[Math.floor(Math.random() * hex.length)]
+  //   }`;
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
