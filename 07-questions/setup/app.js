1803ba102fc7d0170077b3cd12be8e2673ce8ad6
Stefano Frontini <stefanofrontini75@gmail.com>
2022-05-17 12:41:56 +0200

    07-questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,29 @@
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+    // const filter = [...questions].filter((el) => el !== question);
+    // for (let i = 0; i < filter.length; i++) {
+    //   filter[i].classList.remove("show-text");
+    // }
+  });
+});
+
+//
 // traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
